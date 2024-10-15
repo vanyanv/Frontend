3ef36c67e25ba0f11ef98d8f5cd77ada1fb0d0cb
Vardan <vardan.vanyan@gmail.com>
2024-10-15 13:32:31 -0700

    added srtating files for debounce

---
+++ src/Debounce/Debounce.test.ts
@@ -0,0 +1,25 @@
+import debounce from './Debounce';
+import { describe, test, expect } from '@jest/globals';
+
+describe('debounce', () => {
+  test('can be initialized', () => {
+    const increment = debounce(() => {}, 50);
+    expect(increment).toBeTruthy();
+  });
+
+  test('executes after duration', (done) => {
+    let i = 0;
+    const increment = debounce(() => {
+      i++;
+    }, 10);
+
+    expect(i).toBe(0);
+    increment();
+    expect(i).toBe(0);
+
+    setTimeout(() => {
+      expect(i).toBe(1);
+      done();
+    }, 20);
+  });
+});
---
+++ src/Debounce/README.md
@@ -0,0 +1,8 @@
+# Debounce
+
+## Description
+This is the TypeScript file for the project `Debounce`.
+
+## Files
+- `Debounce.ts`: The main TypeScript file.
+
