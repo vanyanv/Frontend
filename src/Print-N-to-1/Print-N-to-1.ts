eadf8bd6f2c83cd34822802ee0f3d06be20dfb89
Vardan <vardan.vanyan@gmail.com>
2024-12-03 16:03:05 -0800

    solved

--- src/Print-N-to-1/Print-N-to-1.ts
+++ src/Print-N-to-1/Print-N-to-1.ts
@@ -5,4 +5,15 @@ function printNto1(i: number, n: number): void {
   printNto1(i - 1, n);
 }
 
-printNto1(6, 6);
+// printNto1(6, 6);
+
+
+function printNto1B(i: number, n: number): void {
+    //base case
+    if (i > n) return;
+    
+    printNto1B(i + 1, n);
+    console.log(i);
+  }
+
+  printNto1B(1,6)
