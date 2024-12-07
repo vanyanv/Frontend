ec7b0e0ce7464122205fefc7c4ac8c08d4ab807b
Vardan <vardan.vanyan@gmail.com>
2024-12-06 16:11:36 -0800

    solved with recursion

--- src/Reverse-Array/Reverse-Array.ts
+++ src/Reverse-Array/Reverse-Array.ts
@@ -1,4 +1,13 @@
 function reverseArray<T>(array: T[], i: number): T[] {
   //base case
   if (i > Math.floor(array.length / 2)) return array;
+
+  //swapping elements in place
+  [array[i], array[array.length - 1 - i]] = [
+    array[array.length - 1 - i],
+    array[i],
+  ];
+  return reverseArray(array, i + 1);
 }
+
+console.log(reverseArray([1, 2, 4, 5, 6], 0));
