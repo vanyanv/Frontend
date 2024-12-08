209374a5d2b18e5626bd71d7f58ca5e1ce966bff
Vardan <vardan.vanyan@gmail.com>
2024-12-08 12:13:20 -0800

    solved w/o recursion

--- src/Reverse-Array/Reverse-Array.ts
+++ src/Reverse-Array/Reverse-Array.ts
@@ -1,3 +1,4 @@
+//recursively
 function reverseArray<T>(array: T[], i: number): T[] {
   //base case
   if (i > Math.floor(array.length / 2)) return array;
@@ -11,3 +12,18 @@ function reverseArray<T>(array: T[], i: number): T[] {
 }
 
 console.log(reverseArray([1, 2, 4, 5, 6], 0));
+
+function reverseArrayR<T>(array: T[]): T[] {
+  //go thru half the array
+  for (let i = 0; i < Math.floor((array.length - 1) / 2); i++) {
+    //swap the elements in place
+    [array[i], array[array.length - 1 - i]] = [
+      array[array.length - 1 - i],
+      array[i],
+    ];
+  }
+
+  return array;
+}
+
+console.log(reverseArrayR([1, 2, 4, 5, 6], 0));
