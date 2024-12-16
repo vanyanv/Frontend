c75f03396601881d34d6e3f0ad4195146bac389e
Vardan <vardan.vanyan@gmail.com>
2024-12-16 15:29:12 -0800

    added solution to when k is larget than the length of the array

--- src/Rotate-Array-by-K-Elements/Rotate-Array-by-K-Elements.ts
+++ src/Rotate-Array-by-K-Elements/Rotate-Array-by-K-Elements.ts
@@ -3,6 +3,9 @@ export function rotateArrayByK(
   k: number,
   direction: string
 ): number[] {
+  //check when k is greater than the length of the array
+  k = k % array.length;
+
   if (array.length < 2) return array;
   const storage =
     direction === 'right'
@@ -18,3 +21,7 @@ export function rotateArrayByK(
 
 console.log(rotateArrayByK([1, 2, 3, 4, 5, 6, 7], 2, 'right'));
 console.log(rotateArrayByK([3, 7, 8, 9, 10, 11], 3, 'left'));
+
+// 0(n) -> time complexity because we are using the spread operator to got thru storage and the array
+// 0(n) -> space complexity because we use storage to hold out numbers and also another array to return the rotated array
+
