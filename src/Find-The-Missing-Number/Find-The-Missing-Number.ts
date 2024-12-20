6e7e379d1a310758d393b680e57c68bce5c76589
Vardan <vardan.vanyan@gmail.com>
2024-12-19 19:08:06 -0800

    solved findMissingNumberOptimally

--- src/Find-The-Missing-Number/Find-The-Missing-Number.ts
+++ src/Find-The-Missing-Number/Find-The-Missing-Number.ts
@@ -19,9 +19,31 @@ export function findMissingNumber(array: number[], n: number): number {
   return -1;
 }
 
+/*
 console.log(findMissingNumber([1, 2, 3, 5], 5));
 console.log(findMissingNumber([1, 2, 4, 5], 5)); // Should return 3
 console.log(findMissingNumber([2, 3, 4, 5], 5)); // Should return 1
 console.log(findMissingNumber([1, 3, 4, 5], 5)); // Should return 2
 console.log(findMissingNumber([1, 2, 3, 4], 5)); // Should return 5
 console.log(findMissingNumber([1, 2, 3, 4, 5], 5)); // Should return undefined (no missing number)
+*/
+
+//O(n) + O(m) + O(h) this is a O(n + m + h ) solution
+
+export function findMissingNumberOptimal(array: number[], n: number): number {
+  const sum1 = array.reduce((sum, curr) => (sum += curr));
+  let sum2 = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum2 += i;
+  }
+
+  return sum2 - sum1;
+}
+
+console.log(findMissingNumberOptimal([1, 2, 3, 5], 5));
+console.log(findMissingNumberOptimal([1, 2, 4, 5], 5)); // Should return 3
+console.log(findMissingNumberOptimal([2, 3, 4, 5], 5)); // Should return 1
+console.log(findMissingNumberOptimal([1, 3, 4, 5], 5)); // Should return 2
+console.log(findMissingNumberOptimal([1, 2, 3, 4], 5)); // Should return 5
+console.log(findMissingNumberOptimal([1, 2, 3, 4, 5], 5)); // Should return undefined (no missing number)s
