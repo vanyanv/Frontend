ed89ae87c3cda8c6a00c0ef23fd2e3a78ddfc04d
Vardan <vardan.vanyan@gmail.com>
2024-12-26 15:44:04 -0800

    used moors voting algo to solve Majority in O(N) and O(1)

--- src/Find-The-Majority-Element/Find-The-Majority-Element.ts
+++ src/Find-The-Majority-Element/Find-The-Majority-Element.ts
@@ -14,6 +14,8 @@ export function findMajority(array: number[], n: number): number {
   for (const [key, value] of storage) {
     if (value > majority) return key;
   }
+
+  return -1;
 }
 
 console.log(findMajority([2, 2, 1, 1, 1, 2, 2], 7));
@@ -22,3 +24,30 @@ console.log(findMajority([4, 4, 2, 4, 3, 4, 4, 3, 2, 4], 10));
 
 //O(n) + O(n)
 //O(n)
+
+//use moors voting law algo to solve
+
+export function findMajorityOptimal(array: number[], n: number): number {
+  let ele: number = array[0];
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (count === 0) {
+      ele = array[i];
+    }
+
+    if (array[i] === ele) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return ele;
+}
+
+//using moors voting law algo
+console.log('MOORS VOTING LAW ');
+console.log(findMajorityOptimal([2, 2, 1, 1, 1, 2, 2], 7));
+console.log(findMajorityOptimal([3, 2, 3], 3));
+console.log(findMajorityOptimal([4, 4, 2, 4, 3, 4, 4, 3, 2, 4], 10));
