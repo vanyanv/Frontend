3113089a3a52616a42bc3fb0c732a61e5a0a4a5f
Vardan <vardan.vanyan@gmail.com>
2024-12-25 16:16:07 -0800

    solved longest-subbarray if the numbers are positive

--- src/Longest-Subbary-With-Given-Sum/Longest-Subbary-With-Given-Sum.ts
+++ src/Longest-Subbary-With-Given-Sum/Longest-Subbary-With-Given-Sum.ts
@@ -24,4 +24,35 @@ console.log(longestSubarray([1, 2, 3, 4, 5], 15));
 console.log(longestSubarray([1, 2, 3, 4, 5], 5));
 console.log(longestSubarray([1, 2, 3, 4, 5], 1));
 
-export function longestSubarrayOptimal(array: number[], k: number): number {}
+export function longestSubarrayOptimal(array: number[], k: number): number {
+  let i = 0;
+  let j = 0;
+  let sum = array[0];
+  let max = 0;
+
+  while (i < array.length) {
+    while (j <= i && sum > k) {
+      sum -= array[j];
+      j++;
+    }
+
+    if (sum === k) {
+      max = Math.max(max, i - j + 1);
+    }
+
+    i++;
+    if (i < array.length) {
+      sum += array[i];
+    }
+  }
+
+  return max;
+}
+
+console.log('OPTIMAL');
+console.log(longestSubarrayOptimal([2, 3, 5], 5));
+console.log(longestSubarrayOptimal([2, 3, 5, 1, 9], 10));
+console.log(longestSubarrayOptimal([1, 2, 3, 4, 5], 9));
+console.log(longestSubarrayOptimal([1, 2, 3, 4, 5], 15));
+console.log(longestSubarrayOptimal([1, 2, 3, 4, 5], 5));
+console.log(longestSubarrayOptimal([1, 2, 3, 4, 5], 1));
