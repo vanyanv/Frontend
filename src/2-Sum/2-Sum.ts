8afd498eb57cab578bf4c91d4be59f7d66a13870
Vardan <vardan.vanyan@gmail.com>
2024-12-25 17:35:52 -0800

    solved 2Sum w/o using a hashmap

--- src/2-Sum/2-Sum.ts
+++ src/2-Sum/2-Sum.ts
@@ -19,3 +19,29 @@ export function twoSum(array: number[], int: number): number[] | undefined {
 
 console.log(twoSum([2, 6, 5, 8, 11], 14));
 console.log(twoSum([2, 6, 5, 8, 11], 15));
+
+export function twoSumOptimal(array: number[], target: number): number[] {
+  let right = 0;
+  let left = array.length - 1;
+
+  //sort the array
+  array.sort((a, b) => a - b);
+
+  while (right < left) {
+    const sum = array[right] + array[left];
+
+    if (sum === target) return [right, left];
+    if (sum < target) {
+      right++;
+    }
+
+    if (sum > target) {
+      left--;
+    }
+  }
+  return [-1, -1];
+}
+console.log('TWO POinter OPTIMAL');
+console.log(twoSumOptimal([2, 6, 5, 8, 11], 14));
+console.log(twoSumOptimal([2, 6, 5, 8, 11], 15));
+
