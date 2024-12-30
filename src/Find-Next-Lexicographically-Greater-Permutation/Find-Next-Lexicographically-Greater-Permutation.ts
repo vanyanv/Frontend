a405f53ead5bead3e45a2f0ad73f2f57736da126
Vardan <vardan.vanyan@gmail.com>
2024-12-29 18:34:15 -0800

    solved using the most optimal solution

--- src/Find-Next-Lexicographically-Greater-Permutation/Find-Next-Lexicographically-Greater-Permutation.ts
+++ src/Find-Next-Lexicographically-Greater-Permutation/Find-Next-Lexicographically-Greater-Permutation.ts
@@ -1,11 +1,46 @@
-# Find Next Lexicographically Greater Permutation
+export function findNext(array: number[]): number[] {
+  let index = -1;
+  //find the index where index i is less than i+1
+  for (let i = array.length - 2; i >= 0; i--) {
+    if (array[i] < array[i + 1]) {
+      index = i;
+      break;
+    }
+  }
 
-Find the next lexicographically greater permutation of numbers in an array of integers.
+  if (index === -1) {
+    return array.reverse();
+  }
 
-## Problem Description
+  //swap the next greatest with out index
+  for (let i = array.length - 1; i > index; i--) {
+    if (array[i] > array[index]) {
+      [array[index], array[i]] = [array[i], array[index]];
+      break;
+    }
+  }
 
-For a given sequence of numbers, find the next sequence that is just larger than the current one when considering all possible permutations. If such an arrangement is not possible, return the numbers sorted in ascending order.
+  //reverse the rest of the array
+  reverseFromIndex(array, index + 1);
 
-## Examples
+  return array;
+}
 
-### Example 1:
+console.log(findNext([1, 3, 2]));
+console.log(findNext([3, 2, 1]));
+
+//Time Complexity -> O(N) + O(N) + O(N)
+//Space Complexity -> O(1)
+
+function reverseFromIndex(arr: number[], startIndex: number) {
+  let left = startIndex;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    // Swap elements
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr;
+}
