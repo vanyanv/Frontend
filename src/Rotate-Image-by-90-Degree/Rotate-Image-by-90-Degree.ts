72a16e1344854ea5b30efa4b0421c869faaf01e3
Vardan <vardan.vanyan@gmail.com>
2025-01-01 16:07:11 -0800

    solved using optimal approach

--- src/Rotate-Image-by-90-Degree/Rotate-Image-by-90-Degree.ts
+++ src/Rotate-Image-by-90-Degree/Rotate-Image-by-90-Degree.ts
@@ -35,3 +35,38 @@ console.log(
 
 //Time Complexity -> O(n) + O(n^2)
 //Space Complexity -> O(n2)
+
+export function rotateOptimal(array: number[][]): number[][] {
+  //transpose the array
+
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i; j < array[i].length; j++) {
+      [array[i][j], array[j][i]] = [array[j][i], array[i][j]];
+    }
+  }
+
+  //reverse each row
+  for (let i = 0; i < array.length; i++) {
+    array[i].reverse();
+  }
+
+  return array;
+}
+
+console.log('Optimal');
+console.log(
+  rotateOptimal([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+
+console.log(
+  rotateOptimal([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ])
+);
