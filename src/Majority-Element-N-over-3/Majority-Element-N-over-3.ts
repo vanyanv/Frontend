58afff0b26c4436dc6490aab44efc267e6fe1704
Vardan <vardan.vanyan@gmail.com>
2025-01-05 16:37:04 -0800

    finished majority element in O(n)

--- src/Majority-Element-N-over-3/Majority-Element-N-over-3.ts
+++ src/Majority-Element-N-over-3/Majority-Element-N-over-3.ts
@@ -40,3 +40,39 @@ export function majoritySumBetter(array: number[], n: number): number[] {
 console.log('better');
 console.log(majoritySumBetter([1, 2, 2, 3, 2], 5));
 console.log(majoritySumBetter([11, 33, 33, 11, 33, 11], 6));
+
+export function majoritySumOptimal(array: number[], n: number): number[] {
+  const k = Math.floor(n / 3);
+  let element1: number | undefined;
+  let count1 = 0;
+  let element2: number | undefined;
+  let count2 = 0;
+  const answer = [];
+  for (let i = 0; i < array.length; i++) {
+    if (count1 === 0 && element1 !== element2) {
+      element1 = array[i];
+    } else if (count2 === 0 && element2 !== element2) {
+      element1 = array[i];
+    } else if (element1 === array[i]) {
+      count1++;
+    } else if (element2 === array[i]) {
+      count2++;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+
+  if (element1 !== undefined && element1 > k) {
+    answer.push(element1);
+  }
+  if (element2 !== undefined && element2 > k) {
+    answer.push(element2);
+  }
+
+  return answer;
+}
+
+console.log('optimal');
+console.log(majoritySumBetter([1, 2, 2, 3, 2], 5));
+console.log(majoritySumBetter([11, 33, 33, 11, 33, 11], 6));
