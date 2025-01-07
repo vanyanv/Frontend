06ecb7a183763f11917a55dda5d2331a3829e7c4
Vardan <vardan.vanyan@gmail.com>
2025-01-07 14:36:10 -0800

    used two pointer to solve optimally

--- src/3-Sum/3-Sum.ts
+++ src/3-Sum/3-Sum.ts
@@ -21,3 +21,62 @@ export function findtriplets(array: number[]): number[][] {
 
 console.log(findtriplets([-1, 0, 1, 2, -1, -4]));
 console.log(findtriplets([-1, 0, 1, 0]));
+
+export function findtripletsBetter(array: number[]): number[][] {
+  const nonDuplicates = new Set<string>();
+  for (let i = 0; i < array.length; i++) {
+    const hashMap = new Map<number, number>();
+    for (let j = i + 1; j < array.length; j++) {
+      const k = -(array[i] + array[j]);
+
+      if (hashMap.has(k)) {
+        nonDuplicates.add(
+          JSON.stringify([array[i], array[j], k].sort((a, b) => a - b))
+        );
+      }
+
+      hashMap.set(array[j], 1);
+    }
+  }
+
+  return [...nonDuplicates].map((ele) => JSON.parse(ele));
+}
+
+console.log('better');
+console.log(findtripletsBetter([-1, 0, 1, 2, -1, -4]));
+console.log(findtripletsBetter([-1, 0, 1, 0]));
+
+export function findtripletsOptimal(array: number[]): number[][] {
+  array.sort((a, b) => a - b);
+  const answer = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i > 0 && array[i] == array[i - 1]) {
+      continue;
+    }
+
+    let j = i + 1;
+    let k = array.length - 1;
+
+    while (j < k) {
+      const sum = array[i] + array[j] + array[k];
+
+      if (sum < 0) {
+        j++;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        answer.push([array[i], array[j], array[k]]);
+        j++;
+        k--;
+        while (j < k && array[j] == array[j - 1]) j++;
+        while (j < k && array[k] == array[k + 1]) k--;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log('optimal');
+console.log(findtripletsBetter([-1, 0, 1, 2, -1, -4]));
+console.log(findtripletsBetter([-1, 0, 1, 2, -1, -4]));
