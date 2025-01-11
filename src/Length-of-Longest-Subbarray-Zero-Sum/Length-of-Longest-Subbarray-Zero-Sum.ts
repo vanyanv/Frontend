3195d91ce217490a9ba4e7c7e65c14e9f62ac938
Vardan <vardan.vanyan@gmail.com>
2025-01-11 15:05:21 -0800

    solved using optimal solution

--- src/Length-of-Longest-Subbarray-Zero-Sum/Length-of-Longest-Subbarray-Zero-Sum.ts
+++ src/Length-of-Longest-Subbarray-Zero-Sum/Length-of-Longest-Subbarray-Zero-Sum.ts
@@ -1,4 +1,4 @@
-export default function longestSubarray(array: number[]): number {
+export function longestSubarray(array: number[]): number {
   let longestSubarray = 0;
 
   for (let i = 0; i < array.length; i++) {
@@ -24,3 +24,30 @@ console.log(longestSubarray([1, 3, -5, 6, -2]));
 
 //Time Complexity -> O(n^2)
 //Space Complexity -> O(N)
+
+export function longestSubarrayOptimal(array: number[]): number {
+  const cache = new Map<number, number>();
+  let maximum = 0;
+
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+
+    if (sum === 0) {
+      maximum = i + 1;
+    } else {
+      if (cache.has(sum)) {
+        maximum = Math.max(maximum, i - (cache.get(sum) ?? 0));
+      } else {
+        cache.set(sum, i);
+      }
+    }
+  }
+  return maximum;
+}
+
+console.log('Optimal');
+console.log(longestSubarrayOptimal([9, -3, 3, -1, 6, -5]));
+console.log(longestSubarrayOptimal([6, -2, 2, -8, 1, 7, 4, -10]));
+console.log(longestSubarrayOptimal([1, 3, -5, 6, -2]));
