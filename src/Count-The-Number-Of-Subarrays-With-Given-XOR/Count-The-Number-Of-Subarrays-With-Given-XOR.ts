3e37cca3a89f0ed4f0eb3071295025b16e283f11
Vardan <vardan.vanyan@gmail.com>
2025-01-12 16:10:04 -0800

    solved using better solution n^2" " ;

--- src/Count-The-Number-Of-Subarrays-With-Given-XOR/Count-The-Number-Of-Subarrays-With-Given-XOR.ts
+++ src/Count-The-Number-Of-Subarrays-With-Given-XOR/Count-The-Number-Of-Subarrays-With-Given-XOR.ts
@@ -23,3 +23,26 @@ console.log('solved using brute force');
 console.log(numberOfSubbarays([4, 2, 2, 6, 4], 6));
 
 console.log(numberOfSubbarays([5, 6, 7, 8, 9], 5));
+
+function numberOfSubbaraysBetter(array: number[], k): number {
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    let currentXor = 0;
+
+    for (let j = i; j < array.length; j++) {
+      currentXor = currentXor ^ array[j];
+
+      if (currentXor === k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log('solved using Better');
+console.log(numberOfSubbaraysBetter([4, 2, 2, 6, 4], 6));
+
+console.log(numberOfSubbaraysBetter([5, 6, 7, 8, 9], 5));
