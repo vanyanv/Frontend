ed26752527b1acf8ee7c4297855424cc7550e5d2
Vardan <vardan.vanyan@gmail.com>
2025-01-20 14:57:16 -0800

    solve using binary search

---
+++ src/Implament-Lower-Bounds/Implament-Lower-Bounds.ts
@@ -0,0 +1,25 @@
+function lowerBounds(array: number[], x: number): number {
+  let start = 0;
+  let end = array.length - 1;
+  let smallest = Infinity;
+  while (smallest > x) {
+    const mid = Math.floor((start + end) / 2);
+    const current = array[mid];
+
+    if (current <= x) {
+      smallest = mid;
+    }
+
+    if (current > x) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return smallest;
+}
+
+console.log('brute force');
+console.log(lowerBounds([1, 2, 2, 3], 2));
+console.log(lowerBounds([3, 5, 8, 15, 19], 9));
